dd09b86d4014366e141bc253e3948b9239ac1245
ZeAmans-Projects <zeaman73@gmail.com>
2026-07-06 11:57:19 +0300

    Fix gallery images

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -72,7 +72,7 @@ export default function ContactPage() {
           <div className="flex justify-center">
 
             <Image
-              src="/images/Gallery/team 1.png"
+              src="/public/images/Gallery/team 1.png"
               alt="Hi Spring Team"
               width={650}
               height={700}
